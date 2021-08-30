117c8a01621b1e3dbc502dc2ce41fe0e3b11bc5a
Gabriel Silva <gabrielsilvadomingues@hotmail.com>
2021-08-30 11:24:54 -0300

    Valor maior, menor e iguais.

---
+++ exercicio41.py
@@ -0,0 +1,9 @@
+print('=' * 15, 'VALOR MAIOR', '=' * 15)
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+if n1>n2:
+    print('O primeiro número é o maior')
+elif n1==n2:
+    print('Os números são iguais')
+else:
+    print('O segundo número é o maior')
