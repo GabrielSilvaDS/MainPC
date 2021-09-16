b376ccf1908e952c005a960753006904fa107f5c
Gabriel Silva <gabrielsilvadomingues@hotmail.com>
2021-09-16 01:19:32 -0300

    Triângulos v2.0

---
+++ exercicio45.py
@@ -0,0 +1,16 @@
+print('=' * 15, 'FORMANDO UM TRIÂNGULO v2.0', '=' * 15)
+# primeiro exercício
+r1 = float(input('Digite uma reta: '))
+r2 = float(input('Digite outra reta: '))
+r3 = float(input('Digite mais uma reta: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('As retas podem formar um triângulo!')
+else:
+    print('As retas não podem formar um triângulo!')
+# Tipos de Triângulos de acordo com os lados.
+if r1 == r2 == r3:
+    print('As retas formam um Equilátero.')
+elif r1 == r2 or r1 == r3 or r2 == r3:
+    print('As retas formam um Isósceles.')
+else:
+    print('As retas formam um Escaleno.')
