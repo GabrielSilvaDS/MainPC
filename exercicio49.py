d55b31c7e68aafbeef4d35627f35607a995b05e6
Gabriel Silva <gabrielsilvadomingues@hotmail.com>
2021-09-19 10:43:44 -0300

    Contagem regressiva v2.0
    alterações bobas

--- exercicio49.py
+++ exercicio49.py
@@ -1,7 +1,8 @@
 print('=' * 15, 'CONTAGEM PRO FIM DE ANO', '=' * 15)
 from time import sleep
+import emoji
 for a in range(10, 0, -1):
     sleep(1)
     print(a)
 sleep(1)
-print('BOOOM!!!')
+print('{} BOOOM!!! {}'.format(emoji.emojize(':fireworks:'), emoji.emojize(':fireworks:')))
