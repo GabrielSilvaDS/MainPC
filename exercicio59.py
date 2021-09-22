98e71df0ebbff9722c7ac5a6644bad5c8c16053f
Gabriel Silva <gabrielsilvadomingues@hotmail.com>
2021-09-22 12:47:17 -0300

    Analisador

---
+++ exercicio59.py
@@ -0,0 +1,24 @@
+print('=' * 15, 'ANALISADOR', '=' * 15)
+soma = 0
+media = 0
+maioridadem = 0
+nomevelho = ''
+mulhermenor = 0
+for a in range(1, 5):
+    print('\033[0;33m-=-\033[m' * 6, '{}ª PESSOA'.format(a), '\033[0;33m-=-\033[m' * 6)
+    nome = str(input('nome: '))
+    idade = int(input('idade: '))
+    sexo = str(input('sexo [M/F]: ')).upper()
+    soma += idade
+    if a == 1 and sexo == 'M':
+        maioridadem = idade
+        nomevelho = nome
+    if sexo == 'M' and idade > maioridadem:
+        maioridadem = idade
+        nomevelho = nome
+    if sexo == 'F' and idade < 20:
+        mulhermenor += 1
+media = soma / 4
+print('Média de idade é {:.0f}'.format(media))
+print('O homem mais velho se chama {} e tem {} anos'.format(nomevelho, maioridadem))
+print('Ao todo são {} mulher(es) com menos de 20 anos'.format(mulhermenor))
